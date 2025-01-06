fb684102c8878e82f5faed911077159bad5490b3
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-06 13:47:35 +0000

    complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(s,n) {
+    if (n<0) {
+        return 'ERROR';
+    }
+    let ans='';
+    for (let i = 0; i < n; i++) {
+        
+        ans += s;
 
+    }
+    return ans;
 };
 
 // Do not edit below this line
