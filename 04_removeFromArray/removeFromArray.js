66492152c920addd4297c7f87d59bd09a1b6b3ea
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-06 14:48:09 +0000

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    for(arg of args){
+        arr = arr.filter(value => value !== arg)
+    }
+    return arr;
+
 };
 
 // Do not edit below this line
