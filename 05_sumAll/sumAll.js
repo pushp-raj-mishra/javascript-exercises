bb25ebfd4c3e395ff25839c746ea52854ffd02ff
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-06 15:03:32 +0000

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a , b) {
+    if(Number.isInteger(a)&&Number.isInteger(b)){
+        if (a>=0&&b>=0) {
+            
+        
+        let sum = 0;
+        for(let i = Math.min(a,b); i <= Math.max(a,b); i++){
+            sum += i;
+        }
+    
+        return sum;
+    }
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
