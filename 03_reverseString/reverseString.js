5765ed9bb0959403f4f642090b192303fce02653
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-06 13:53:54 +0000

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let ans = '';
+    let n = s.length;
+    for (let i = n - 1; i >=0; i--){
+        ans += s[i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
