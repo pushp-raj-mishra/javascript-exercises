3b773c8136fe0a1a4978884dcc80e7e99c06762b
PUSHP RAJ <he.is.pushp@gmail.com>
2025-01-06 15:08:31 +0000

    complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(n) {
+    if ((n%4==0)) {
+        if (n%100==0&&n%400==0) {
+            return true;
+        } else if (n%100!=0) {
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
